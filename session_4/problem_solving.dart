aad778d1768b213c3e8e2fcfe20d7020f8141e0c
Ahmad Abdullah <ahmadabdullah141@yahoo.com>
2025-03-20 21:37:11 +0200

    done

--- session_4/problem_solving.dart
+++ session_4/problem_solving.dart
@@ -17,8 +17,7 @@ void main() {
   List<int> nums1 = [1, 2, 3, 1];
   List<int> nums2 = [1, 2, 3, 4];
   List<int> nums3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
-
-  print(containsDuplicate(nums1)); // Output: true
-  print(containsDuplicate(nums2)); // Output: false
-  print(containsDuplicate(nums3)); // Output: true
+  print(containsDuplicate(nums1));
+  print(containsDuplicate(nums2));
+  print(containsDuplicate(nums3));
 }
